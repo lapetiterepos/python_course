6df854d67a6e165a1395103a32677740658e0295
khanimood <khanimood@gmail.com>
2022-03-07 12:34:27 +0300

    lecture 4 uploaded

---
+++ lectures/lecture4/.ipynb_checkpoints/access_template_argv-checkpoint.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+interface = argv[1]
+vlan = argv[2]
+
+access_template = ['switchport mode access',
+                   'switchport access vlan {}',
+                   'switchport nonegotiate',
+                   'spanning-tree portfast',
+                   'spanning-tree bpduguard enable']
+
+print('interface {}'.format(interface))
+print('\n'.join(access_template).format(vlan))
